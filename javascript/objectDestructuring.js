76fb9e620f81c67dee0f615fa83dd614371da700
[kasagala dayanand] <[angularexpertdayanand@gmail.com]>
2024-02-08 01:13:53 +0530

    Object destructuring has been committed

---
+++ javascript/objectDestructuring.js
@@ -0,0 +1,31 @@
+//Extract properties in Object Destructuring.
+const person = {name : 'Alice', age : 25, city : 'Wonderland'};
+const {name , age} = person;
+console.log("get data through Object destructuring =>",name,age);
+
+//Default values in Object Destructuring.
+const settings = {fontSize : 16, theme : 'light'};
+const {fontSize, theme, language = 'en'} = settings;
+console.log("pulling the values from the object => ", language);
+
+//Extracting Properties from Nested Objects.
+const company = {name1 : 'TechCo', info: { employees : 100, location : 'Silicon Valley'}};
+const {name1, info: {employees, location}} = company;
+console.log("pulling values from nested objects => ", name1, employees, location);
+
+//Renaming properties in Destructuring.
+const book = {title : 'Web Dev', author : 'Farooq dad'};
+const {title : bookTitle, author: bookAuthor} = book;
+console.log("Renaming properties through Destructuring => ",bookTitle, bookAuthor);
+
+//Rest parameter in Object Destructuring.
+const userDetails = {username : 'jsCoder', email: 'angularexpertdayanand@gmail.com', age : 38};
+const {username , ...otherDetails} = userDetails;
+console.log("Rest parameter is involved in the destructuring => ",username, otherDetails)
+
+//Object Destructuring in the function parameters
+const personData = {firstName : 'Dayanand', lastName : 'Kumar', country : 'USA'};
+function displayFullName({firstName, lastName}){
+    console.log("destructuring in the function parametes => ", `${firstName} ${lastName}`);
+}
+displayFullName(personData)
